441d3eaaa850137147d6957216f58bedadbffce3
jssuzanne <jssuzanne@anybox.fr>
2018-05-31 11:00:38 +0200

    Fix import

--- anyblok_attachment/bloks/attachment/tests/test_historized.py
+++ anyblok_attachment/bloks/attachment/tests/test_historized.py
@@ -6,7 +6,7 @@
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
 from anyblok.tests.testcase import BlokTestCase
-from anyblok.bloks.anyblok_core.exceptions import (
+from anyblok_mixins.mixins.exceptions import (
     ForbidUpdateException, ForbidDeleteException)
 from os import urandom
 
